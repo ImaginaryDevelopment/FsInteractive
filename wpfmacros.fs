488902f80bb27497d7d474964ad9d165d257dbe9
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-09-22 14:48:09 -0400

    fixing wpf macro to handle display of items

--- wpfmacros.fs
+++ wpfmacros.fs
@@ -39,8 +39,69 @@ module WpfEventLoop =
 
 [<AutoOpen>]
 module wpfmacros =
+  open System
+  open System.IO
+  open System.Linq
+  open System.Xml
   open System.Windows
+  open System.Windows.Controls
+  open System.Windows.Data
+  open System.Windows.Markup
   WpfEventLoop.Install()
-  let simpleWindow () = 
+
+  let HelloWpfWindow () =
     let window = new Window(Title="Simple Test",Width = 800., Height = 600.)
+    window.Show()
+  (* let createDataTemplate value = // http://stackoverflow.com/a/17106767/57883
+    let xmlFormat = sprintf """<DataTemplate DataType="DataGrid"><TextBlock Text="Hello"/></DataTemplate>"""
+    let xml = xmlFormat // typeof<TextBlock>.Name // value
+    let context = ParserContext()
+    context.XamlTypeMapper <- new XamlTypeMapper(Array.empty)
+    // context.XamlTypeMapper.AddMappingProcessingInstruction(System.String.empty, "http://schemas.microsoft.com/winfx/2006/xaml/presentation")
+    // context.XamlTypeMapper.AddMappingProcessingInstruction("x", "http://schemas.microsoft.com/winfx/2006/xaml")
+    context.XmlnsDictionary.Add(System.String.Empty,"http://schemas.microsoft.com/winfx/2006/xaml/presentation")
+    context.XmlnsDictionary.Add("x","http://schemas.microsoft.com/winfx/2006/xaml")
+    context.XmlnsDictionary.Add("System","clr-namespace:System;assembly=mscorlib")
+    printfn "xml: %s" xml
+    let xaml = XamlReader.Parse(xml,context)
+    printfn "%A" xaml
+    let template = DataTemplate(xaml)
+    printfn "%A" template
+    template
+  *)
+  (* let createTemplate = 
+    let templ = """<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'><Grid><TextBlock Text="Hello"/></Grid></DataTemplate>"""
+    printfn "templ: %s" templ
+    use xmlReader = XmlReader.Create(new StringReader(templ))
+    let xaml = XamlReader.Load(xmlReader)
+    printfn "Xamlreader finished %A" xaml
+    DataGridTemplateColumn(xaml) 
+  *)
+  let createTemplate = // http://stackoverflow.com/questions/8779893/create-datagridtemplatecolumn-through-c-sharp-code
+    let binding = Binding("Changes", Mode=BindingMode.OneWay)
+    // let textFactory = new FrameworkElementFactory(typeof<TextBlock>)
+    let listFactory = new FrameworkElementFactory(typeof<ListView>)
+    listFactory.SetBinding(ListView.ItemsSourceProperty,binding)
+    // panelFactory.AppendChild(textFactory)
+    let textTemplate = DataTemplate(VisualTree=listFactory)
+    textTemplate
+
+  let display<'a> (source:'a seq) =
+    let window = new Window(Title="Data display", Width = 800., Height = 600.)
+    let grid = new DataGrid()
+    // interesting: (DataTemplate)System.Windows.Markup.XamlReader.Parse(dt);
+    let changesColumn = 
+      let col = DataGridTemplateColumn()
+      col.Header<-"File Changes"
+      let template = 
+        // createTemplate typeof<string> typeof<TextBlock>
+        createTemplate
+      // template.DataType <- typeof<DataGridTemplateColumn>
+      printfn "about to set dataDataGridTemplateColumn's cell template %A" template
+      col.CellTemplate <- template
+      col
+    grid.Columns.Add(changesColumn)
+    grid.AutoGeneratingColumn.Add (fun e-> if e.PropertyName ="Changes" then e.Cancel <- true)
+    grid.ItemsSource <- source
+    window.Content <- grid 
     window.Show()
