0e9a223b17e0625f0afb2ffa3f63286e0fee9057
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-09-30 10:04:42 -0400

    starting to learn fslex

---
+++ MacroRunner/HelloFsLex/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module HelloFsLex.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("HelloFsLex")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("brandon.dimperio")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ MacroRunner/HelloFsLex/Component1.fs
@@ -0,0 +1,5 @@
+﻿namespace HelloFsLex
+
+type Class1() = 
+    member this.X = "F#"
+
