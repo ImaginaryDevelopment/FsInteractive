44e22fd2e25a48c8e338f75e7ae71bf74257e25b
bdimpe@gmail <bdimpe@gmail>
2017-02-06 11:45:47 -0500

    adding in a MEF project

---
+++ MacroRunner/BMeffers/Library1.fs
@@ -0,0 +1,14 @@
+﻿namespace BMeffers
+
+open System.ComponentModel.Composition
+open MacroRunner.Schema
+[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module VolumeDownEffects =
+    ()
+
+[<Export(typeof<ISideEffect>)>]
+type VolumeDownEffect() = 
+    interface ISideEffect with
+        member x.Description = "Turn the Volume Down when run"
+        member x.Execution _ = printfn "Turned volume down, maybe"
+        member x.Name = "VolumeDown"
---
+++ MacroRunner/MacroRunner/Schema.fs
@@ -0,0 +1,12 @@
+﻿module MacroRunner.Schema
+
+type ISideEffect<'T> = 
+    abstract Name:string
+    abstract Description: string
+    abstract Execution: 'T -> unit
+type ISideEffect = 
+    inherit ISideEffect<unit>
+
+
+
+
