b8324cd5a7000ce7250b0cf514df860b03917f78
bdimpe@gmail <imaginarydevelopment@gmail.com>
2018-10-30 09:18:50 -0400

    wip non-compiling

--- CodeGeneration/DataModelToC.fs
+++ CodeGeneration/DataModelToC.fs
@@ -4,12 +4,33 @@ open System
 open BReusable
 open BReusable.StringHelpers
 
+type Indentable = int*string
+type Indentables = Indentable list
+type Comments = Indentables
+// for interfaces
 type Property = {
             Name:string
             Type:string
-            Comment:string
+            Comment:Comments
             IsWritable:bool
 }
+
+type ReadWriteType =
+    | Readonly
+    | Writable of field:string option
+
+type PropertyGenType =
+    | Auto of ReadWriteType
+    | Custom of body:Indentables*field: Indentable option
+
+// for classes
+type PropertyGenArgument = {
+    Name:string
+    Type:string
+    Comments:Comments
+    PropertyGenType:PropertyGenType
+}
+
 module Impl = // generators without the indenter/delimiter
     let generateInheritanceDeclaration inheritance =
         inheritance
@@ -24,10 +45,9 @@ module Impl = // generators without the indenter/delimiter
     let generateClassDeclaration typeName inheritance =
         generateInheritanceDeclaration inheritance
         |> sprintf "public class %s%s" typeName
-    let generateProperty isPublic prop =
+    let generateAutoProperty isPublic (prop:Property):Indentables =
         [
-            if isValueString prop.Comment then
-                yield 1, sprintf "// %s" prop.Comment
+            yield! prop.Comment
             yield 1,
                 if prop.IsWritable then
                     "{ get; set; }"
@@ -55,7 +75,7 @@ module Impl = // generators without the indenter/delimiter
                 |> List.map (fun (prop:Property) ->
                     if isReadOnly && prop.IsWritable then {prop with IsWritable = false} else prop
                 )
-                |> List.collect(generateProperty usePublicProps)
+                |> List.collect(generateAutoProperty usePublicProps)
             yield 0,"}"
         ]
         |> generateBase indenter
@@ -69,3 +89,25 @@ let generateInterface typeName inheritance isReadOnly props indenter =
 let generateClass typeName inheritance isReadOnly props indenter =
     generateClassDeclaration typeName inheritance
     |> generateType Class isReadOnly indenter props
+
+let generateInpcClass typeName inheritance props indenter =
+    let decl = generateClassDeclaration typeName ("INotifyPropertyChanged"::inheritance)
+    let props =
+        props
+        |> List.map(
+            // generate default get/set
+            function
+            |{PropType=AutoImplemented Writable} as prop ->
+                let get = sprintf "get { return _%s; }" prop.Name
+                let set =  [
+                    2,"set"
+                    2,"{"
+                    3, sprintf "_%s = value;" prop.Name
+                    3, sprintf "this.RaisePropertyChanged(%s)" prop.Name
+                    2,"}"
+                ]
+                {prop with PropType = Body ([2,get],set)}
+
+            | prop -> prop
+        )
+    generateType Class false indenter props decl
